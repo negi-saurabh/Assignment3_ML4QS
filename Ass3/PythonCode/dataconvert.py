268621ad96798c9f16ee35b5230d446372edaadb
Saurabh Negi <saurabhnegi@Saurabhs-MacBook-Pro.local>
2019-06-19 22:32:03 +0200

    v2

--- Ass3/PythonCode/dataconvert.py
+++ Ass3/PythonCode/dataconvert.py
@@ -15,30 +15,33 @@ with open(input_Data_path + 'log.txt', "rb") as file_pipe:
 df_csv = pd.read_csv(output_Data_path+ 'MT.csv', error_bad_lines=False)
 for i, g in df_csv.groupby('sensorName'):
     print (g['value'])
-    g.to_csv('../Ass3-DataOutputfiles/{}.csv'.format(i), index_label=False)
+    g.to_csv('../Ass3-DataOutputfiles/{}.csv'.format(i), index=False)
 
 
 # cleaning and separating values for Accelerometer
-df_csv = pd.read_csv(output_Data_path+ 'accelerometer-kx023.csv', error_bad_lines=False)
+df_csv = pd.read_csv(output_Data_path+ 'accelerometer-kx023.csv', index_col=[0])
 df_csv['value'] = df_csv['value'].str.replace(']', '')
 df_csv['value'] = df_csv['value'].str.replace('[', '')
 df_csv[['x','y','z']] = df_csv.value.str.split(',', expand=True)
 df_csv = df_csv.drop('value',axis = 1)
+df_csv['timestamp'] = df_csv['timestamp']*1000000
 df_csv.to_csv(output_Data_path + 'final_accelerometer.csv')
 
 # cleaning and separating values for Magnetometer
-df_csv = pd.read_csv(output_Data_path+ 'mag-akm09918.csv', error_bad_lines=False)
+df_csv = pd.read_csv(output_Data_path+ 'mag-akm09918.csv',index_col=[0])
 df_csv['value'] = df_csv['value'].str.replace(']', '')
 df_csv['value'] = df_csv['value'].str.replace('[', '')
 df_csv[['x','y','z']] = df_csv.value.str.split(',', expand=True)
 df_csv = df_csv.drop('value',axis = 1)
+df_csv['timestamp'] = df_csv['timestamp']*1000000
 df_csv.to_csv(output_Data_path + 'final_magnetometer.csv')
 
 # cleaning and separating values for Gyroscope (Orientation for now)
-df_csv = pd.read_csv(output_Data_path + 'orientation.csv', error_bad_lines=False)
+df_csv = pd.read_csv(output_Data_path + 'orientation.csv', index_col=[0])
 df_csv['value'] = df_csv['value'].str.replace(']', '')
 df_csv['value'] = df_csv['value'].str.replace('[', '')
 df_csv[['x','y','z']] = df_csv.value.str.split(',', expand=True)
 df_csv = df_csv.drop('value',axis = 1)
+df_csv['timestamp'] = df_csv['timestamp']*1000000
 df_csv.to_csv(output_Data_path + 'final_orientation.csv')
 
